1e144d8e527c78ff161ef239d6ace5b2624d02c9
sankalp120 <sankalpjhamb10@gmail.com>
2025-12-18 01:29:51 +0530

    added threejs rendering of earth and asteroids with correct api calls

--- backend/neo_api.py
+++ backend/neo_api.py
@@ -15,7 +15,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "http://127.0.0.1:5501",
+        "http://127.0.0.1:8000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
